de98175e5b1c6688b288f78864f708294f98208e
nerdcomplex <brshapiro@bshapiro14.local>
2014-05-06 08:20:54 -0400

    Test commit

---
+++ PegJumper/PegJumperTests/PegJumperTests.m
@@ -0,0 +1,34 @@
+//
+//  PegJumperTests.m
+//  PegJumperTests
+//
+//  Created by brshapiro on 5/6/14.
+//
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PegJumperTests : XCTestCase
+
+@end
+
+@implementation PegJumperTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
